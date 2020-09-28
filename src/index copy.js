57933284d45d83a0277bd340697acce645fe6018
Zain <zainiscoding@gmail.com>
2020-09-28 16:32:45 +0900

    Removed copy of index.js

--- src/index copy.js
+++
@@ -1,9 +0,0 @@
-import { renderPage } from "./page-load.js";
-import { todoFactory } from "./todo-object.js";
-import { projectFactory } from "./project-object.js";
-import { storeProjects } from "./storage.js";
-import { renderProjectList } from "./render-projects.js";
-import { renderToDoObjects } from "./render-todo-objects.js";
-
-
-console.log("wait");
