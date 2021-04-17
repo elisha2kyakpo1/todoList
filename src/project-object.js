d6e1148814d3b2b0d85886d80f751dc7747e64d8
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-04-18 01:46:01 +0200

    Fix Module dependancy cycle

--- src/project-object.js
+++ src/project-object.js
@@ -12,45 +12,47 @@ const projectFactory = (title) => {
     const removeFromProjectList = (todoObject) => {
         projectTodoList.splice(projectTodoList.indexOf(todoObject), 1);
     }
+  };
 
-    const removeProjectFromList = (project) => {
-        projectList.splice(projectList.indexOf(project), 1);
-    };
-
-    return {
-        title,
-        projectTodoList,
-        projectTodoListTitles,
-        addToProjectTodoList,
-        removeFromProjectList,
-    };
+  return {
+    title,
+    projectTodoList,
+    projectTodoListTitles,
+    addToProjectTodoList,
+    removeFromProjectList,
+  };
 };
 
-//Project objects stored in a list
+// Project objects stored in a list
 let projectObjectList = [];
 
-//Create the project
+// Create the project
 const createProjects = () => {
-    storeProjects.getProjectList();
-    const projectObjectListTitles = [];
-    projectObjectList.forEach((projectObject) => {
-        projectObjectListTitles.push(projectObject.title);
-    });
-    storeProjects.projectList.forEach((project) => {
-        if (!projectObjectListTitles.includes(project)) {
-            const newProject = projectFactory(project);
-            return projectObjectList.push(newProject);
-        }
-    });
-}
-
-// Remove projects 
-const removeProject = (project) => {
-    if (projectObjectList.length > 1) {
-        projectObjectList.splice(projectObjectList.indexOf(project), 1);
-    } else if (projectObjectList.length == 1) {
-        projectObjectList = [];
+  storeProjects.getProjectList();
+  const projectObjectListTitles = [];
+  projectObjectList.forEach((projectObject) => {
+    projectObjectListTitles.push(projectObject.title);
+  });
+  storeProjects.projectList.forEach((project) => {
+    if (!projectObjectListTitles.includes(project)) {
+      const newProject = projectFactory(project);
+      return projectObjectList.push(newProject);
     }
-}
+  });
+};
 
-export { projectFactory, projectObjectList, createProjects, removeProject };
+// Remove projects
+const removeProject = (project) => {
+  if (projectObjectList.length > 1) {
+    projectObjectList.splice(projectObjectList.indexOf(project), 1);
+  } else if (projectObjectList.length === 1) {
+    projectObjectList = [];
+  }
+};
+
+export {
+  projectFactory,
+  projectObjectList,
+  createProjects,
+  removeProject,
+};
