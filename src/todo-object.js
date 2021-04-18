15963c0f817176bf5fb6458afffdd8dafbe41f84
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-04-18 02:57:12 +0200

    Style the header

--- src/todo-object.js
+++ src/todo-object.js
@@ -1,56 +1,55 @@
-import { projectObjectList } from "./project-object.js";
+import { projectObjectList } from './project-object';
 
 const todoFactory = (
+  title,
+  dueDate,
+  priority,
+  note = 'None',
+  doneStatus = 'Incomplete',
+) => {
+  const todoInfo = [title, dueDate, priority, note, doneStatus];
+
+  return {
     title,
     dueDate,
     priority,
-    note = "None",
-    doneStatus = "Incomplete"
-) => {
-    let todoInfo = [title, dueDate, priority, note, doneStatus];
-
-    return {
-        title,
-        dueDate,
-        priority,
-        note,
-        doneStatus,
-        todoInfo,
-    };
+    note,
+    doneStatus,
+    todoInfo,
+  };
 };
 
 const createTodos = () => {
-    projectObjectList.forEach((project) => {
-        if (project.projectTodoListTitles.length > 0) {
-            project.projectTodoListTitles.forEach((title) => {
-                const localTodos = localStorage.getItem(
-                    project.title + " " + title + " todo info"
-                );
+  projectObjectList.forEach((project) => {
+    if (project.projectTodoListTitles.length > 0) {
+      project.projectTodoListTitles.forEach((title) => {
+        const localTodos = localStorage.getItem(
+          project.title`  ${title}  todo info`,
+        );
 
-                if (localTodos != "" && localTodos != undefined) {
-                    const todoInfo = localStorage[
-                        project.title + " " + title + " todo info"
-                    ].split(",");
-                    let newTodo = todoFactory(
-                        todoInfo[0],
-                        todoInfo[1],
-                        todoInfo[2],
-                        todoInfo[3],
-                        todoInfo[4]
-                    );
+        if (localTodos !== '' && localTodos !== undefined) {
+          const todoInfo = localStorage[project.title` ${title} todo info`
+          ].split(',');
+          let newTodo = todoFactory(
+            todoInfo[0],
+            todoInfo[1],
+            todoInfo[2],
+            todoInfo[3],
+            todoInfo[4],
+          );
 
-                    const projectTodoTitles = [];
-                    project.projectTodoList.forEach((todo) => {
-                        projectTodoTitles.push(todo.title);
-                    });
+          const projectTodoTitles = [];
+          project.projectTodoList.forEach((todo) => {
+            projectTodoTitles.push(todo.title);
+          });
 
-                    if (!projectTodoTitles.includes(title)) {
-                        project.addToProjectTodoList(newTodo);
-                    }
-                }
-            });
+          if (!projectTodoTitles.includes(title)) {
+            project.addToProjectTodoList(newTodo);
+          }
         }
-    });
-}
+      });
+    }
+  });
+};
 
 export { todoFactory, createTodos };
